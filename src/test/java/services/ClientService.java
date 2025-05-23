06bad283967728b39e9f1be6c4348854e6a78a80
Alex-Matasa <alexandru.matasa86@gmail.com>
2025-05-23 15:24:39 +0300

    Enhance framework with logging and assertion utilities

--- src/test/java/services/ClientService.java
+++ src/test/java/services/ClientService.java
@@ -3,6 +3,7 @@ package services;
 import base.BaseRequestSpec;
 import endpoints.EndpointPaths;
 import io.restassured.response.Response;
+import logger.LoggerUtility;
 import models.request.User;
 
 import static io.restassured.RestAssured.given;
@@ -10,9 +11,16 @@ import static io.restassured.RestAssured.given;
 public class ClientService {
 
     public static Response registerClient(User user) {
-        return given()
+        LoggerUtility.info("Registering client with email: " + user.getClientEmail());
+
+        Response response = given()
                 .spec(BaseRequestSpec.withoutAuth())
                 .body(user)
                 .post(EndpointPaths.REGISTER_CLIENT);
+
+        LoggerUtility.info("Received response: Status " + response.getStatusCode());
+        LoggerUtility.info("Response body: " + response.asPrettyString());
+
+        return response;
     }
 }
--- src/test/java/utils/AssertionUtils.java
+++ src/test/java/utils/AssertionUtils.java
@@ -1,6 +1,7 @@
 package utils;
 
 import io.restassured.response.Response;
+import logger.LoggerUtility;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -9,10 +10,12 @@ public class AssertionUtils {
 
     public static void assertStatusCode(Response response, int expectedStatusCode) {
         assertThat(response.getStatusCode(), equalTo(expectedStatusCode));
+        LoggerUtility.info("Validated Status code: " + expectedStatusCode);
     }
 
     public static void assertErrorMessage(Response response, String expectedMessage) {
         String actualMessage = response.jsonPath().getString(ResponseFields.ERROR);
         assertThat(actualMessage, equalTo(expectedMessage));
+        LoggerUtility.info("Validated Error message");
     }
 }
